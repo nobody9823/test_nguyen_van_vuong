c8d9d7a86229d0a2a2b2a2ccfd22107fd59a2007
arakakitatsuki <20yuteo@gmail.com>
2021-06-18 18:02:09 +0900

    fix table name to user plans billing

--- backend/database/migrations/2021_06_18_132053_create_user_plans_billing_table.php
+++ backend/database/migrations/2021_06_18_132053_create_user_plans_billing_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserReturnBillingTable extends Migration
+class CreateUserPlansBillingTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUserReturnBillingTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_return_billing', function (Blueprint $table) {
+        Schema::create('user_plans_billing', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
             $table->integer('return_id');
@@ -29,6 +29,6 @@ class CreateUserReturnBillingTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_return_billing');
+        Schema::dropIfExists('user_plans_billing');
     }
 }
