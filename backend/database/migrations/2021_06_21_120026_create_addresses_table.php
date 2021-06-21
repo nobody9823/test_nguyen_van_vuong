575511e4d84418799c19eea424f54494098f59fe
ShinnosukeTakagi <s.takagi5664@gmail.com>
2021-06-21 21:44:20 +0900

    Fix addresses_table migration file
    feature/CU-9hwbwb

--- backend/database/migrations/2021_06_21_120026_create_addresses_table.php
+++ backend/database/migrations/2021_06_21_120026_create_addresses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserAddressesTable extends Migration
+class CreateAddressesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUserAddressesTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_addresses', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->string('postal_code');
@@ -33,6 +33,6 @@ class CreateUserAddressesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_addresses');
+        Schema::dropIfExists('addresses');
     }
 }
