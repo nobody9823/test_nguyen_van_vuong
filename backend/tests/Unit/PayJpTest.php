78d55eea9840c32690c584e84ff61f5244db6a73
arakakitatsuki <20yuteo@gmail.com>
2021-06-29 19:23:26 +0900

    add test for pay jp test

---
+++ backend/tests/Unit/PayJpTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Exception;
+use App\Actions\PayJp\PayJp;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PayJpTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->pay_jp = new PayJp();
+    }
+
+    public function testSuccessPayment()
+    {
+        \Payjp\Payjp::setApiKey(config('app.pay_jp_secret_for_test'));
+
+        $params = [
+            'card' => [
+                "number" => "4242424242424242",
+                "exp_month" => "12",
+                "exp_year" => "2024",
+            ]
+        ];
+        $token = \Payjp\Token::create($params, $options = ['payjp_direct_token_generate' => 'true']);
+        $response = $this->pay_jp->Payment(1000, $token->id);
+        $this->assertSame(true, is_string($response->id));
+    }
+
+    public function testFailByCardDeclined()
+    {
+        $this->expectException(Exception::class);
+        \Payjp\Payjp::setApiKey(config('app.pay_jp_secret_for_test'));
+
+        $params = [
+            'card' => [
+                "number" => "4000000000080319",
+                "exp_month" => "12",
+                "exp_year" => "2024",
+            ]
+        ];
+        $token = \Payjp\Token::create($params, $options = ['payjp_direct_token_generate' => 'true']);
+        ($this->pay_jp->Payment(1000, $token->id))->execute(1);
+    }
+
+    public function testFailByExpiredCard()
+    {
+        $this->expectException(Exception::class);
+        \Payjp\Payjp::setApiKey(config('app.pay_jp_secret_for_test'));
+
+        $params = [
+            'card' => [
+                "number" => "4000000000004012",
+                "exp_month" => "12",
+                "exp_year" => "2024",
+            ]
+        ];
+        $token = \Payjp\Token::create($params, $options = ['payjp_direct_token_generate' => 'true']);
+        ($this->pay_jp->Payment(1000, $token->id))->execute(1);
+    }
+
+    public function testFailByCardDeclinedByInvalidExpirationDate()
+    {
+        $this->expectException(Exception::class);
+        \Payjp\Payjp::setApiKey(config('app.pay_jp_secret_for_test'));
+
+        $params = [
+            'card' => [
+                "number" => "4000000000000077",
+                "exp_month" => "12",
+                "exp_year" => "2024",
+            ]
+        ];
+        $token = \Payjp\Token::create($params, $options = ['payjp_direct_token_generate' => 'true']);
+        ($this->pay_jp->Payment(1000, $token->id))->execute(1);
+    }
+}
