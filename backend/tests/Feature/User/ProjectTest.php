91c4ae67029896d74333b087fcda2374608d6ec1
arakakitatsuki <20yuteo@gmail.com>
2021-06-29 19:40:15 +0900

    fix project test to login users

--- backend/tests/Feature/User/ProjectTest.php
+++ backend/tests/Feature/User/ProjectTest.php
@@ -7,6 +7,7 @@ use App\Models\Project;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class ProjectTest extends TestCase
@@ -33,7 +34,8 @@ class ProjectTest extends TestCase
 
     public function testShowAction()
     {
-        $response = $this->get(route('user.project.show', ['project' => Project::first()]));
+        $response = $this->actingAs($this->users->first())
+            ->get(route('user.project.show', ['project' => Project::first()]));
         $response->assertOk();
     }
 
