5901534451d217a2bbdce6ae8bc07f0ca39b7404
adgjmptwgw <adgjmptwgw@gmail.com>
2021-07-04 18:56:14 +0900

    Fixed routing for replying comments.
    feature/CU-9vw3rg

--- backend/routes/admin.php
+++ backend/routes/admin.php
@@ -80,7 +80,7 @@ Route::middleware('auth:admin')->group(function () {
     Route::resource('comment', CommentController::class, ['only' => ['index', 'show', 'destroy']]);
 
     //返信管理
-    Route::resource('reply', ActivityReportController::class, ['only' => ['index', 'create', 'edit', 'show', 'destroy']]);
+    Route::resource('reply', ReplyController::class, ['only' => ['create', 'store', 'edit', 'update', 'destroy']]);
 
     // メッセージ管理
     Route::resource('message', MessageController::class)->only(['index','show']);
