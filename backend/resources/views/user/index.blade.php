28eea3609f9656d066391110046a17cdde65175e
adgjmptwgw <adgjmptwgw@gmail.com>
2021-07-05 23:55:14 +0900

    Implemented to read JS files
    feature/CU-9zynub

--- backend/resources/views/user/index.blade.php
+++ backend/resources/views/user/index.blade.php
@@ -526,40 +526,14 @@ use Carbon\Carbon;
             </div>
             <div class="footer-over_R_03"><i class="fas fa-chevron-right"></i></div>
         </a>
-        <button>ボタン</button>
     </div>
 @endsection
 
 @section('script')
 <script>
-// プロジェクトのお気に入り登録・解除処理
-    $('.liked_project').on('click', function() {
-        var el = $(this);
-        var projectId = el.attr('id');
-
-        el.append('<meta name="csrf-token" content="{{ csrf_token() }}">');
-
-        $.ajax({
-            url: '/project/'+ projectId + '/liked',
-            type: 'POST',
-            data: {'project_id': projectId },
-            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-        })
-        .then((response) => {
-            console.log(response);
-            if(response == "登録"){
-                el.children('i').attr('class', 'fas fa-heart');
-            } else if(response == "削除"){
-                el.children('i').attr('class', 'far fa-heart');
-            } else if (response == "未ログイン") {
-                alert("ログインもしくは会員登録をしてください");
-            } else {
-                alert("エラーが起こりました。");
-            }
-        })
-        .fail((error)=>{
-            console.log("エラーが起こりました。")
-        })
-    });
+// csrfトークンを下記のproject-liked.jsファイルに送信
+window.Laravel = {};
+window.Laravel.csrfToken = @json( csrf_token() );
 </script>
+<script src="{{ asset('/js/project-liked.js') }}"></script>
 @endsection
