ed200149b3f64de51a293a34190c7566ddcb0f83
ShinnosukeTakagi <s.takagi5664@gmail.com>
2021-07-07 00:53:07 +0900

    Implement to display youtube movie at project.show
    feature/CU-9zyjvg

--- backend/database/factories/ProjectFileFactory.php
+++ backend/database/factories/ProjectFileFactory.php
@@ -22,13 +22,22 @@ class ProjectFileFactory extends Factory
      */
     public function definition()
     {
+        $image_file = [
+            'file_url' => 'public/sampleImage/now_printing.png',
+            'file_content_type' => 'image_url',
+        ];
+        $video_file = [
+            'file_url' => 'https://www.youtube.com/embed/HB4QrJyWqEw',
+            'file_content_type' => 'video_url',
+        ];
+        $file_faker = Arr::random([
+            $image_file,
+            $video_file
+        ]);
         return [
             'project_id' => $this->faker->numberBetween(1, 50),
-            'file_url' => 'public/sampleImage/now_printing.png',
-            'file_content_type' => Arr::random([
-                'image_url',
-                'video_url'
-            ])
+            'file_url' => $file_faker['file_url'],
+            'file_content_type' => $file_faker['file_content_type'],
         ];
     }
 }
