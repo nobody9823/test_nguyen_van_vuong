02957bd18a9c042a96b33b6ab11a3f683469b52c
arakakitatsuki <20yuteo@gmail.com>
2021-07-06 21:38:22 +0900

    fix plan payment card not to select returns witch is out of stock

--- backend/resources/views/components/user/plan/plan-payment-card.blade.php
+++ backend/resources/views/components/user/plan/plan-payment-card.blade.php
@@ -11,20 +11,21 @@
             <div class="wlr_64_R">
                 <div class="as_01">
                     <div class="as_check">
-                        <input type="checkbox" name="plan_ids[]" class="plan_ids ac_list_checks checkbox-fan" onChange="Plans.planIsChecked(this)" id="{{ $plan->id }}" value="{{ $plan->price }}">
+                        <input type="checkbox" name="plan_ids[]" class="plan_ids ac_list_checks checkbox-fan" onChange="Plans.planIsChecked(this)" id="{{ $plan->id }}" value="{{ $plan->price }}" {{ ($plan->limit_of_supporters > 0) === false ? 'disabled' : '' }}>
                         <label for="{{ $plan->id }}" class="checkbox-fan_02">{{ $plan->price }}円</label>
                     </div>
                 </div>
-
-                <div class="as_02">
-                    <div class="cp_ipselect_02 cp_chb ">
-                        <select name="plans[{{$plan->id}}]amount[]" id="plan_amount_{{ $plan->id }}" onChange="Plans.planAmountIsChanged(this)" disabled>
-                            @for($i = 1; $i <= $plan->limit_of_supporters; $i ++)
-                                <option value="{{ $i }}">数量{{ $i }}</option>
-                            @endfor
-                        </select>
-                    </div>
-                </div><!--/-->
+                @if ($plan->limit_of_supporters > 0)
+                    <div class="as_02">
+                        <div class="cp_ipselect_02 cp_chb ">
+                            <select name="plans[{{$plan->id}}]amount[]" id="plan_amount_{{ $plan->id }}" onChange="Plans.planAmountIsChanged(this)" disabled>
+                                @for($i = 1; $i <= $plan->limit_of_supporters; $i ++)
+                                    <option value="{{ $i }}">数量{{ $i }}</option>
+                                @endfor
+                            </select>
+                        </div>
+                    </div><!--/-->
+                @endif
 
                 <div class="as_tit">{{ $plan->title }}</div><!--/-->
 
