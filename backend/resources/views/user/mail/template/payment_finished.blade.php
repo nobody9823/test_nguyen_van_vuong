bb3b3f5015b239b0b032482112eca5837e6942ae
arakakitatsuki <20yuteo@gmail.com>
2021-07-06 18:56:03 +0900

    add payment finished page

---
+++ backend/resources/views/user/mail/template/payment_finished.blade.php
@@ -0,0 +1,11 @@
+@extends('user.layouts.mail_template')
+
+@section('content')
+    <p>
+        <div>Fan Returnです。</div>
+        <div>ご支援ありがとうございます。</div>
+        <div>あなたは、{{ $billing_users_count }}人目の支援者です<br>支援総額は{{ $achievement_amount }}円になりました</div>
+        <div>プロジェクト名 : {{ $project_title }}</div>
+        <div>支援ID : {{ $payment_id }}</div>
+    </p>
+@endsection
