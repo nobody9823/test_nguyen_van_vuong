0f477816140dd2a7de69f8168e20136828b2a244
ShinnosukeTakagi <s.takagi5664@gmail.com>
2021-07-08 12:18:15 +0900

    Tweak
    feature/CU-a20dev

--- backend/resources/views/user/layouts/base.blade.php
+++ backend/resources/views/user/layouts/base.blade.php
@@ -57,7 +57,7 @@
                         @if(isset(Auth::user()->profile))
                         <div class="user_btn_01_01" style="background-image: url({{ Storage::url(Auth::user()->profile->image_url) }})">
                         @else
-                        <div class="user_btn_01_01" style="background-image: url({{ Storage::url(Auth::user()->profile->image_url) }})">
+                        <div class="user_btn_01_01" style="background-image: url('/image/')">
                         @endif
                         </div>
                     </a>
@@ -223,7 +223,7 @@
                             @if(isset(Auth::user()->profile))
                             <div class="user_btn_01_01" style="background-image: url({{ Storage::url(Auth::user()->profile->image_url) }})">
                             @else
-                            <div class="user_btn_01_01" style="background-image: url({{ Storage::url(Auth::user()->profile->image_url) }})">
+                            <div class="user_btn_01_01" style="background-image: url('image/my-page.svg')">
                             @endif
                             </div>
                         </a>
