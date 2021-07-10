8a5517888c7c4592781e020357e72f806c823af4
adgjmptwgw <adgjmptwgw@gmail.com>
2021-07-10 22:54:23 +0900

    The number of supporters and the total amount of support are displayed on the project details on the management screen, the payment completion screen, and the email after payment.
    feature/CU-a5xqrp

--- backend/resources/views/components/manage/project/show.blade.php
+++ backend/resources/views/components/manage/project/show.blade.php
@@ -149,9 +149,9 @@
                                                     {{ number_format($project->target_amount) }}円
                                                 </h5>
                                                 <h3>支援者総数 : </h3>
-                                                <h5 class="mb-3">{{ $project->payment_users_count }}人</h5>
+                                                <h5 class="mb-3">{{ $project->payments_count }}人</h5>
                                                 <h3>支援金総額 : </h3>
-                                                <h5 class="mb-3">{{ number_format($project->achievement_amount) }}
+                                                <h5 class="mb-3">{{ number_format($project->payments_sum_price) }}
                                                     円</h5>
                                                 <h3>目標額達成率 : </h3>
                                                 <h5 class="mb-3">{{ $project->getAchievementRate }}％</h5>
