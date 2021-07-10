3de209cc339f346aebc95ab05c91ba60fddb12d8
adgjmptwgw <adgjmptwgw@gmail.com>
2021-07-10 18:17:21 +0900

    added ps_plan_content column in the Projects Table
    feature/CU-a5xfq1

--- backend/database/migrations/2021_06_18_115050_create_projects_table.php
+++ backend/database/migrations/2021_06_18_115050_create_projects_table.php
@@ -20,6 +20,7 @@ class CreateProjectsTable extends Migration
             $table->foreignId('user_id')->constrained('users');
             $table->string('title')->default('');
             $table->string('content', 15000)->default('');
+            $table->string('ps_plan_content', 5000)->default('');
             $table->integer('target_amount')->default(0);
             $table->string('curator')->default('');
             $table->date('start_date')->default(Carbon::minValue());
