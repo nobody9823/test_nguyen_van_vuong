8e458fb90b60cf04d6c655345afc0820964bc0f4
kosei-program-dev <kosei.program.dev@gmail.com>
2021-07-10 14:21:30 +0900

    Tweak (format)
    #CU-a03jjy

--- backend/app/Http/Controllers/Admin/SupporterPurchaseController.php
+++ backend/app/Http/Controllers/Admin/SupporterPurchaseController.php
@@ -27,7 +27,7 @@ class SupporterPurchaseController extends Controller
      */
     // public function create()
     // {
-        //
+    //
     // }
 
     /**
@@ -38,7 +38,7 @@ class SupporterPurchaseController extends Controller
      */
     // public function store(Request $request)
     // {
-        //
+    //
     // }
 
     /**
@@ -49,7 +49,7 @@ class SupporterPurchaseController extends Controller
      */
     // public function show($id)
     // {
-        //
+    //
     // }
 
     /**
@@ -60,7 +60,7 @@ class SupporterPurchaseController extends Controller
      */
     // public function edit($id)
     // {
-        //
+    //
     // }
 
     /**
@@ -72,7 +72,7 @@ class SupporterPurchaseController extends Controller
      */
     // public function update(Request $request, $id)
     // {
-        //
+    //
     // }
 
     /**
@@ -83,7 +83,7 @@ class SupporterPurchaseController extends Controller
      */
     // public function destroy($id)
     // {
-        //
+    //
     // }
 
     /**
