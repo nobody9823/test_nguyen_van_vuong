cac8936d0715f8bdac47770d33ca3e0056bb2325
ShinnosukeTakagi <s.takagi5664@gmail.com>
2021-07-14 18:20:50 +0900

    Drop bank_accounts_table and create identifications_table
    feature/CU-a9wj57

---
+++ backend/database/migrations/2021_07_14_181252_create_identifications_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Enums\BankAccountType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIdentificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('identifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('bank_code')->comment('金融機関コード、先頭に０が入る可能性あるのでstring、4桁バリデーション');
+            $table->string('branch_code')->comment('支店コード、先頭に０が入る可能性あるのでstring、3桁バリデーション');
+            $table->enum('account_type', BankAccountType::getValues());
+            $table->string('account_number')->comment('口座番号、先頭に０が入る可能性あるのでstring、7桁バリデーション');
+            $table->string('account_name')->comment('カタカナで姓名記入');
+            $table->string('identify_image_1');
+            $table->string('identify_image_2');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('identifications');
+    }
+}
