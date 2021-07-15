f230427cd06e3c0a327dde4cbd220ab9de9f2406
arakakitatsuki <20yuteo@gmail.com>
2021-07-15 18:30:00 +0900

    fix create action of my page controller to create new project and project file

--- backend/app/Http/Controllers/User/MyProjectController.php
+++ backend/app/Http/Controllers/User/MyProjectController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\ProjectFile;
+use App\Models\Plan;
 use App\Models\Tag;
+use Carbon\Carbon;
 use Auth;
 use Illuminate\Http\Request;
 
@@ -28,7 +31,28 @@ class MyProjectController extends Controller
      */
     public function create()
     {
-        //
+        $tags = Tag::pluck('name', 'id');
+
+        $project = Auth::user()->projects()
+                    ->save(Project::make(
+                        [
+                            'title' => '',
+                            'content' => '',
+                            'ps_plan_content' => '',
+                            'target_amount' => 0,
+                            'curator' => '',
+                            'start_date' => Carbon::minValue(),
+                            'end_date' => Carbon::maxValue(),
+                            'release_status' => '---',
+                        ])
+                    );
+
+        $project->projectFiles()->save(ProjectFile::make([
+            'file_url' => 'public/sampleImage/now_printing.png',
+            'file_content_type' => 'image_url',
+        ]));
+
+        return view('user.my_project.edit', ['project' => $project, 'tags' => $tags]);
     }
 
     /**
