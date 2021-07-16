149cc3c977dd28dda11da29a6ef15c60f264098c
ShinnosukeTakagi <s.takagi5664@gmail.com>
2021-07-17 01:34:17 +0900

    Implement PurifierCast
    feature/CU-abzjbn

---
+++ backend/app/Casts/PurifierCast.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Mews\Purifier\Facades\Purifier;
+
+class PurifierCast implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function get($model, $key, $value, $attributes)
+    {
+        return Purifier::clean($value);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function set($model, $key, $value, $attributes)
+    {
+        return $value;
+    }
+}
