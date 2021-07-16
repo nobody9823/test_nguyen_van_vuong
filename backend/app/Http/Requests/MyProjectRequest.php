9b61b510e4f4b62f35b842cbb997400cdb79596e
arakakitatsuki <20yuteo@gmail.com>
2021-07-16 17:39:51 +0900

    fix my project request to validation overview page

--- backend/app/Http/Requests/MyProjectRequest.php
+++ backend/app/Http/Requests/MyProjectRequest.php
@@ -25,70 +25,38 @@ class MyProjectRequest extends FormRequest
     public function rules(Request $request)
     {
         return [
+            'title' => ['nullable', 'string', 'max:255'],
+            'content' => ['nullable', 'string', 'max:5000'],
+            'tags' => ['nullable', 'array'],
+            'tags.*' => ['nullable', 'string'],
             'target_amount' => ['nullable', 'integer', 'min:0'],
-            'start_date' => ['required', 'date_format:Y-m-d H:i', 'after_or_equal:'.$request->route('project')->start_date],
-            'end_date' => ['required', 'date_format:Y-m-d H:i', 'after:start_date', 'before_or_equal:'.$request->route('project')->end_date],
+            'start_date' => ['nullable', 'date_format:Y-m-d H:i', 'after_or_equal:'.$request->route('project')->start_date],
+            'end_date' => ['nullable', 'date_format:Y-m-d H:i', 'after:start_date', 'before_or_equal:'.$request->route('project')->end_date],
             'ps_plan_content' => ['nullable', 'string', 'max:2000'],
         ];
     }
 
     protected function prepareForValidation()
     {
-        if ($this->start_month === null){
+        if ($this->has('title') && $this->title === null){
+            $this->offsetUnset('title');
+        }
+
+        if ($this->has('content') && $this->input('content') === null){
+            $this->offsetUnset('content');
+        }
+
+        if ($this->has('start_date')){
             $this->merge([
-                'start_month' => date('m', strtotime('1 day'))
+                'start_date' => $this->start_year.'-'.$this->start_month.'-'.$this->start_day.' '.$this->start_hour.':'.$this->start_minute
             ]);
         }
 
-        if ($this->start_day === null){
+        if ($this->has('end_date')){
             $this->merge([
-                'start_day' => date('d', strtotime('1 day'))
+                'end_date' => $this->end_year.'-'.$this->end_month.'-'.$this->end_day.' '.$this->end_hour.':'.$this->end_minute
             ]);
         }
-
-        if ($this->start_hour === null){
-            $this->merge([
-                'start_hour' => date('H', strtotime('1 day'))
-            ]);
-        }
-
-        if ($this->start_minute === null){
-            $this->merge([
-                'start_minute' => date('i', strtotime('1 day'))
-            ]);
-        }
-
-        $this->merge([
-            'start_date' => $this->start_year.'-'.$this->start_month.'-'.$this->start_day.' '.$this->start_hour.':'.$this->start_minute
-        ]);
-
-        if ($this->end_month === null){
-            $this->merge([
-                'end_month' => date('m', strtotime('2 day'))
-            ]);
-        }
-
-        if ($this->end_day === null){
-            $this->merge([
-                'end_day' => date('d', strtotime('2 day'))
-            ]);
-        }
-
-        if ($this->end_hour === null){
-            $this->merge([
-                'end_hour' => date('H', strtotime('2 day'))
-            ]);
-        }
-
-        if ($this->end_minute === null){
-            $this->merge([
-                'end_minute' => date('i', strtotime('2 day'))
-            ]);
-        }
-
-        $this->merge([
-            'end_date' => $this->end_year.'-'.$this->end_month.'-'.$this->end_day.' '.$this->end_hour.':'.$this->end_minute
-        ]);
 
         if($this->ps_plan_content === null){
             $this->merge([
