6ce0967f43276193c08bfe1fb2215311b207ed7b
arakakitatsuki <20yuteo@gmail.com>
2021-07-16 15:12:27 +0900

    fix target amount page to hold input date

--- backend/resources/views/components/user/my_project/target_amount.blade.php
+++ backend/resources/views/components/user/my_project/target_amount.blade.php
@@ -1,78 +1,91 @@
+<form action="{{ route('user.project.update', ['project' => $project]) }}" method="post">
+    @csrf
+    @method('PUT')
 <div class="form_item_row">
         <div class="form_item_tit">目標金額<span class="hissu_txt">必須</span></div>
-        <input type="number" name="postal_code" class="p-postal-code def_input_100p" value="" placeholder="（例）100000">
-    </div><!--/form_item_row-->
+        <input type="number" name="target_amount" class="p-postal-code def_input_100p" value="{{ old('target_amount', optional($project)->target_amount) }}" placeholder="（例）100000">
+    </div>
 
     <div class="form_item_row">
         <div class="form_item_tit">掲載開始日(日付、時刻)<span class="hissu_txt">必須</span></div>
         <div class="cp_ipselect cp_normal" style="margin-right: 10px;">
-            <select id="birth_year" class="form-control" name="birth_year" readonly>
-                <option value="{{ date('Y') }}">{{ date('Y') }}</option>
+            <select id="birth_year" class="form-control" name="start_year" readonly>
+            @for($i = (int) date('Y'); $i <= (int) date('Y') + 2; $i ++)
+                <option value="{{ $i }}" {{ old('start_year', optional(optional($project)->start_date)->year) == $i ? 'selected' : '' }}>{{ $i }}</option>
+            @endfor
             </select>
         </div>
         <div class="cp_ipselect cp_normal" style="margin-right: 10px;">
-            <select id="birth_month" class="form-control" name="birth_month">
-                @for ($i = 1; $i <= 12; $i++)
-                <option value="{{ $i }}" {{ date('mm') == $i ? selected : '' }}>{{ $i }}</option>
+            <select id="birth_month" class="form-control" name="start_month">
+                @for ($i = 0; $i <= 12; $i++)
+                <option value="{{ sprintf('%02d', $i) }}" {{ old('start_month', optional(optional($project)->start_date)->month) == sprintf('%02d', $i) ? 'selected' : '' }}>{{ $i }}</option>
                 @endfor
             </select>
         </div>
         <div class="cp_ipselect cp_normal" style="margin-right: 10px;">
-            <select id="birth_day" class="form-control" name="birth_day">
-                @for ($i = 1; $i <= 31; $i++)
-                <option value="{{ $i }}">{{ $i }}</option>
+            <select id="birth_day" class="form-control" name="start_day">
+                @for ($i = 0; $i <= date('d', strtotime('last day of' . now())); $i++)
+                <option value="{{ sprintf('%02d', $i) }}" {{ old('start_day', optional(optional($project)->start_date)->day) == sprintf('%02d', $i) ? 'selected' : '' }}>{{ $i }}</option>
                 @endfor
             </select>
         </div>
         <div class="cp_ipselect cp_normal" style="margin-right: 10px;">
-            <select id="hour" class="form-control" name="hour">
+            <select id="hour" class="form-control" name="start_hour">
                 @for ($i = 0; $i <= 23; $i++)
-                <option value="{{ $i }}">{{ $i }}</option>
+                <option value="{{ sprintf('%02d', $i) }}" {{ old('start_hour', optional(optional($project)->start_date)->hour) == sprintf('%02d', $i) ? 'selected' : '' }}>{{ $i }}</option>
                 @endfor
             </select>
         </div>
         <div class="cp_ipselect cp_normal" >
-            <select id="minute" class="form-control" name="minute">
-                @for ($i = 0; $i <= 60; $i++)
-                <option value="{{ $i }}">{{ $i }}</option>
+            <select id="minute" class="form-control" name="start_minute">
+                @for ($i = 0; $i <= 59; $i++)
+                <option value="{{ sprintf('%02d', $i) }}" {{ old('start_minute', optional(optional($project)->start_date)->minute) == sprintf('%02d', $i) ? 'selected' : '' }}>{{ $i }}</option>
                 @endfor
             </select>
         </div>
-    </div><!--/form_item_row-->
+    </div>
 
     <div class="form_item_row">
         <div class="form_item_tit">掲載終了日(日付、時刻)<span class="hissu_txt">必須</span></div>
         <div class="cp_ipselect cp_normal" style="margin-right: 10px;">
-            <select id="birth_year" class="form-control" name="birth_year" readonly>
-                <option value="{{ date('Y') }}">{{ date('Y') }}</option>
+            <select id="birth_year" class="form-control" name="end_year" readonly>
+            @for($i = (int) date('Y'); $i <= (int) date('Y') + 2; $i ++)
+                <option value="{{ $i }}" {{ old('end_year', optional(optional($project)->end_date)->year) == $i ? 'selected' : '' }}>{{ $i }}</option>
+            @endfor
             </select>
         </div>
         <div class="cp_ipselect cp_normal" style="margin-right: 10px;">
-            <select id="birth_month" class="form-control" name="birth_month">
-                @for ($i = 1; $i <= 12; $i++)
-                <option value="{{ $i }}" {{ date('mm') == $i ? selected : '' }}>{{ $i }}</option>
-                @endfor
+            <select id="birth_month" class="form-control" name="end_month">
+            @for ($i = 0; $i <= 12; $i++)
+                <option value="{{ sprintf('%02d', $i) }}" {{ old('end_month', optional(optional($project)->end_date)->month) == sprintf('%02d', $i) ? 'selected' : '' }}>{{ $i }}</option>
+            @endfor
             </select>
         </div>
         <div class="cp_ipselect cp_normal" style="margin-right: 10px;">
-            <select id="birth_day" class="form-control" name="birth_day">
-                @for ($i = 1; $i <= 31; $i++)
-                <option value="{{ $i }}">{{ $i }}</option>
-                @endfor
+            <select id="birth_day" class="form-control" name="end_day">
+            @for ($i = 0; $i <= date('d', strtotime('last day of' . now())); $i++)
+                <option value="{{ sprintf('%02d', $i) }}" {{ old('end_day', optional(optional($project)->end_date)->day) == sprintf('%02d', $i) ? 'selected' : '' }}>{{ $i }}</option>
+            @endfor
             </select>
         </div>
         <div class="cp_ipselect cp_normal" style="margin-right: 10px;">
-            <select id="hour" class="form-control" name="hour">
+            <select id="hour" class="form-control" name="end_hour">
                 @for ($i = 0; $i <= 23; $i++)
-                <option value="{{ $i }}">{{ $i }}</option>
+                <option value="{{ sprintf('%02d', $i) }}" {{ old('end_hour', optional(optional($project)->end_date)->hour) == sprintf('%02d', $i) ? 'selected' : '' }}>{{ $i }}</option>
                 @endfor
             </select>
         </div>
         <div class="cp_ipselect cp_normal" >
-            <select id="minute" class="form-control" name="minute">
+            <select id="minute" class="form-control" name="end_minute">
                 @for ($i = 0; $i <= 60; $i++)
-                <option value="{{ $i }}">{{ $i }}</option>
+                <option value="{{ sprintf('%02d', $i) }}" {{ old('end_minute', optional(optional($project)->end_date)->minute) == sprintf('%02d', $i) ? 'selected' : '' }}>{{ $i }}</option>
                 @endfor
             </select>
         </div>
-    </div><!--/form_item_row-->
+        <div class="def_btn">
+            <button type="submit" class="disable-btn">
+                <p style="font-size: 1.8rem;font-weight: bold;color: #fff;">保存する</p>
+            </button>
+        </div>
+    </div>
+</form>
