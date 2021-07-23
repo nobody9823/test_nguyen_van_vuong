28df00ee4bc6652d10fb86d6a05969c0659b0e5d
Yuzuki-9 <s1f102000624@iniad.org>
2021-07-23 17:57:16 +0900

    tweak
    CU-9zynmg

--- backend/database/migrations/2021_07_23_175631_change_prefecture_at_addresses_table.php
+++ backend/database/migrations/2021_07_23_175631_change_prefecture_at_addresses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangePrefectureAtAddressTable extends Migration
+class ChangePrefectureAtAddressesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class ChangePrefectureAtAddressTable extends Migration
      */
     public function up()
     {
-        Schema::table('address', function (Blueprint $table) {
+        Schema::table('addresses', function (Blueprint $table) {
             $table->string('prefecture')->default('')->change();
         });
     }
@@ -25,7 +25,7 @@ class ChangePrefectureAtAddressTable extends Migration
      */
     public function down()
     {
-        Schema::table('address', function (Blueprint $table) {
+        Schema::table('addresses', function (Blueprint $table) {
             $table->string('prefecture')->change();
         });
     }
