bdb57a758fb12a5eda542ce00d3753f4f757a67e
ShinnosukeTakagi <s.takagi5664@gmail.com>
2021-07-25 18:10:32 +0900

    Fix to display current value
    feature/CU-9zynmg

--- backend/resources/views/components/user/mypage/profile-form.blade.php
+++ backend/resources/views/components/user/mypage/profile-form.blade.php
@@ -132,7 +132,7 @@
                     <option value="">年</option>
                     <?php $years = array_reverse(range(today()->year - 100, today()->year)); ?>
                     @foreach($years as $year)
-                        <option value="{{ $year }}" {{ old('year') == $year ? 'selected' : '' }}>{{ $year }}</option>
+                        <option value="{{ $year }}" {{ old('year', Auth::user()->profile->getYearOfBirth()) == $year ? 'selected' : '' }}>{{ $year }}</option>
                     @endforeach
                 </select>
             </div>
@@ -141,7 +141,7 @@
                 <select id="month" name="month">
                     <option value="">月</option>
                     @foreach(range(1, 12) as $month)
-                        <option value="{{ $month }}" {{ old('month') == $month ? 'selected' : '' }}>{{ $month }}</option>
+                        <option value="{{ $month }}" {{ old('month', Auth::user()->profile->getMonthOfBirth()) == $month ? 'selected' : '' }}>{{ $month }}</option>
                     @endforeach
                 </select>
             </div>
@@ -151,8 +151,8 @@
             </div>
             <div class="cp_ipselect cp_normal">
                 <select name="birthday_is_published">
-                    <option value="1">公開する</option>
-                    <option value="0" >公開しない</option>
+                    <option value="1" {{ old('birthday_is_published', Auth::user()->profile->birthday_is_published) == "1" ? 'selected' : '' }}>公開する</option>
+                    <option value="0" {{ old('birthday_is_published', Auth::user()->profile->birthday_is_published) == "0" ? 'selected' : '' }}>公開しない</option>
                 </select>
             </div>
         </div>
@@ -171,15 +171,15 @@
 			<div class="prof_edit_editbox pee_select_hori">
 				<div class="cp_ipselect cp_normal">
 					<select name="gender">
-                        <option value="女性">女性</option>
-						<option value="男性">男性</option>
-                        <option value="その他">その他</option>
+                        <option value="女性" {{ old('gender', Auth::user()->profile->gender) === "女性" ? 'selected' : '' }}>女性</option>
+                        <option value="男性" {{ old('gender', Auth::user()->profile->gender) === "男性" ? 'selected' : '' }}>男性</option>
+                        <option value="その他" {{ old('gender', Auth::user()->profile->gender) === "その他" ? 'selected' : '' }}>その他</option>
 					</select>
 				</div>
 				<div class="cp_ipselect cp_normal">
 					<select name="gender_is_published">
-                        <option value="1">公開する</option>
-                        <option value="0">公開しない</option>
+                        <option value="1" {{ old('gender_is_published', Auth::user()->profile->gender_is_published) == "1" ? 'selected' : '' }}>公開する</option>
+                        <option value="0" {{ old('gender_is_published', Auth::user()->profile->gender_is_published) == "0" ? 'selected' : '' }}>公開しない</option>
                     </select>
 				</div>
 			</div>
@@ -195,7 +195,7 @@
         <div class="prof_edit_row">
 			<div class="prof_edit_01">自己紹介<br><span>編集中</span></div>
 			<div class="prof_edit_editbox">
-				<textarea rows="8" name="introduction"></textarea>
+				<textarea rows="8" name="introduction">{{ old('introduction', Auth::user()->profile->introduction) }}</textarea>
 			</div>
 			<div class="prof_edit_03">
                 <a href="javascript:document.introductionForm.submit()">更新</a>
@@ -237,18 +237,20 @@
     </div>
     <div class="prof_edit_row">
         <div class="prof_edit_01">URL</div>
-        {{-- <div class="prof_edit_02">{{ Auth::user()->sns_links->twitter_url }}</div>
-        <div class="prof_edit_02">{{ Auth::user()->sns_links->instagram_url }}</div>
-        <div class="prof_edit_02">{{ Auth::user()->sns_links->youtube_url }}</div>
-        <div class="prof_edit_02">{{ Auth::user()->sns_links->tiktok_url }}</div>
-        <div class="prof_edit_02">{{ Auth::user()->sns_links->other_url }}</div> --}}
+        {{-- <div class="prof_edit_02">{{ Auth::user()->snsLink->twitter_url }}</div>
+        <div class="prof_edit_02">{{ Auth::user()->snsLink->instagram_url }}</div>
+        <div class="prof_edit_02">{{ Auth::user()->snsLink->youtube_url }}</div>
+        <div class="prof_edit_02">{{ Auth::user()->snsLink->tiktok_url }}</div>
+        <div class="prof_edit_02">{{ Auth::user()->snsLink->other_url }}</div> --}}
         <div class="prof_edit_03">
             編集
             <a href="{{ route('user.profile', ['input_type' => 'sns_links']) }}" class="cover_link"></a></div>
     </div>
     <div class="prof_edit_row">
         <div class="prof_edit_01">現在地</div>
-        <div class="prof_edit_02">{{ optional(Auth::user()->address)->prefecture }}</div>
+        <div class="prof_edit_02">
+            {{ optional(Auth::user()->address)->prefecture }}
+        </div>
         <div class="prof_edit_03">
             編集
             <a href="{{ route('user.profile', ['input_type' => 'prefecture_id']) }}" class="cover_link"></a></div>
@@ -256,10 +258,10 @@
     <div class="prof_edit_row">
         <div class="prof_edit_01">生年月日</div>
         <div class="prof_edit_02">
-            @if(isset(Auth::user()->profile->birthday))
-                {{ Auth::user()->profile->birthday }}
-            @else
+            @if(Auth::user()->profile->birthday === '0001-01-01')
                 設定されていません
+            @else
+                {{ Auth::user()->profile->birthday }}
             @endif
         </div>
         <div class="prof_edit_03">編集<a href="{{ route('user.profile', ['input_type' => 'birthday']) }}" class="cover_link"></a></div>
