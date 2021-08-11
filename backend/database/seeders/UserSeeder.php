0fc5d74089d835a38f67109642370ec19f33d1bb
adgjmptwgw <adgjmptwgw@gmail.com>
2021-08-11 17:00:50 +0900

    Corrected dummy data and built relationships

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Address;
 use App\Models\Identification;
-use App\Models\Payment;
+use App\Models\Comment;
+use App\Models\Reply;
 use App\Models\Profile;
 use App\Models\SnsLink;
 use Illuminate\Database\Seeder;
@@ -24,12 +25,14 @@ class UserSeeder extends Seeder
             $user->identification()->save(Identification::factory()->make());
             $user->profile()->save(Profile::factory()->make());
             $user->snsLink()->save(SnsLink::factory()->make());
+            $user->comments()->saveMany(Comment::factory(rand(1,10))->hasReply(rand(0,1))->create());
         });
 
         User::factory(100)->create()->each(function ($user) {
             $user->address()->save(Address::factory()->make());
             $user->identification()->save(Identification::factory()->make());
             $user->profile()->save(Profile::factory()->make());
+            $user->comments()->saveMany(Comment::factory(rand(1,10))->hasReply(rand(0,1))->create());         
         });
     }
 }
