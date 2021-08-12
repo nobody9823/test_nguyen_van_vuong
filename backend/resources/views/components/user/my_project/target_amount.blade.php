dec32ed938895ece2782d5d729d75af7b33fd909
arakakitatsuki <20yuteo@gmail.com>
2021-08-12 16:00:12 +0900

    fix start date and end date input at target amount page

--- backend/resources/views/components/user/my_project/target_amount.blade.php
+++ backend/resources/views/components/user/my_project/target_amount.blade.php
@@ -33,57 +33,8 @@
                 ※審査期間があるため、2週間以降の日付を設定してください。
             </span>
         </div>
-        <div class="publish_date_wrapper">
-            <div class="cp_ipselect cp_normal">
-                <select class="form-control" id="start_year" name="start_year" onchange="updateMyProject.checkDateIsFilled(this, {{ $project->id }})">
-                    <option value='' disabled selected style='display:none;'>年</option>
-                    @for($i = (int) date('Y'); $i <= (int) date('Y') + 2; $i ++)
-                    <option value="{{ $i }}"
-                        {{ old('start_year', $project->start_date->year) == sprintf('%02d', $i) ? 'selected' : '' }}>
-                        {{ $i }}
-                    </option>
-                    @endfor
-                </select>
-            </div>
-            <div class="publish_date_after_month">
-                <div class="cp_ipselect cp_normal cp_width_50">
-                    <select class="form-control" id="start_month" name="start_month" onchange="updateMyProject.checkDateIsFilled(this, {{ $project->id }})">
-                        <option value='' disabled selected style='display:none;'>月</option>
-                        @for ($i = 1; $i <= 12; $i++)
-                        <option value="{{ sprintf('%02d', $i) }}"
-                            {{ old('start_month', $project->start_date->month) == sprintf('%02d', $i) ? 'selected' : '' }}>
-                            {{ $i }}
-                        </option>
-                        @endfor
-                    </select>
-                </div>
-                <div class="cp_ipselect cp_normal cp_width_50">
-                    <select class="form-control" id="start_day" name="start_day" data-old-value="{{ old('start_day', $project->start_date->day) }}" onchange="updateMyProject.checkDateIsFilled(this, {{ $project->id }})"></select>
-                </div>
-                <div class="cp_ipselect cp_normal cp_width_50">
-                    <select class="form-control" id="start_hour" name="start_hour" onchange="updateMyProject.checkDateIsFilled(this, {{ $project->id }})">
-                        <option value='' disabled selected style='display:none;'>時</option>
-                        @for ($i = 0; $i <= 23; $i++)
-                        <option value="{{ sprintf('%02d', $i) }}"
-                            {{ old('start_hour', $project->start_date->hour) == sprintf('%02d', $i) ? 'selected' : '' }}>
-                            {{ $i }}
-                        </option>
-                        @endfor
-                    </select>
-                </div>
-                <div class="cp_ipselect cp_normal cp_width_50">
-                    <select class="form-control" id="start_minute" name="start_minute" onchange="updateMyProject.checkDateIsFilled(this, {{ $project->id }})">
-                        <option value='' disabled selected style='display:none;'>分</option>
-                        @for ($i = 0; $i <= 59; $i++)
-                        <option value="{{ sprintf('%02d', $i) }}"
-                            {{ old('start_minute', $project->start_date->minute) == sprintf('%02d', $i) ? 'selected' : '' }}>
-                            {{ $i }}
-                        </option>
-                        @endfor
-                    </select>
-                </div>
-            </div>
-        </div>
+        <input type="text" id="start_date" name="start_date" class="p-postal-code def_input_100p"
+            value="{{ old('start_date', optional($project)->start_date) }}" placeholder="（例）100000" oninput="updateMyProject.textInput(this, {{ $project->id }})">
     </div>
 
     <div class="form_item_row">
@@ -103,57 +54,8 @@
                 ※掲載期間は最長60日で設定してください
             </span>
         </div>
-        <div class="publish_date_wrapper">
-            <div class="cp_ipselect cp_normal">
-                <select class="form-control" id="end_year" name="end_year" onchange="updateMyProject.checkDateIsFilled(this, {{ $project->id }})">
-                    <option value='' disabled selected style='display:none;'>年</option>
-                    @for($i = (int) date('Y'); $i <= (int) date('Y') + 2; $i ++)
-                    <option value="{{ $i }}"
-                        {{ old('end_year', $project->end_date->year) == sprintf('%02d', $i) ? 'selected' : '' }}>{{ $i }}
-                    </option>
-                    @endfor
-                </select>
-            </div>
-            <div class="publish_date_after_month">
-                <div class="cp_ipselect cp_normal cp_width_50">
-                    <select class="form-control" id="end_month" name="end_month" onchange="updateMyProject.checkDateIsFilled(this, {{ $project->id }})">
-                        <option value='' disabled selected style='display:none;'>月</option>
-                        @for ($i = 1; $i <= 12; $i++)
-                        <option value="{{ sprintf('%02d', $i) }}"
-                            {{ old('end_month', $project->end_date->month) == sprintf('%02d', $i) ? 'selected' : '' }}>
-                            {{ $i }}
-                        </option>
-                        @endfor
-                    </select>
-                </div>
-                <div class="cp_ipselect cp_normal cp_width_50">
-                    <select class="form-control" id="end_day" name="end_day" data-old-value="{{ old('end_day', $project->end_date->day) }}" onchange="updateMyProject.checkDateIsFilled(this, {{ $project->id }})"></select>
-                </div>
-                <div class="cp_ipselect cp_normal cp_width_50">
-                    <select class="form-control" name="end_hour" id="end_hour" onchange="updateMyProject.checkDateIsFilled(this, {{ $project->id }})">
-                        <option value='' disabled selected style='display:none;'>時</option>
-                        @for ($i = 0; $i <= 23; $i++)
-                        <option value="{{ sprintf('%02d', $i) }}"
-                            {{ old('end_hour', $project->end_date->hour) == sprintf('%02d', $i) ? 'selected' : '' }}>
-                            {{ $i }}
-                        </option>
-                        @endfor
-                    </select>
-                </div>
-                <div class="cp_ipselect cp_normal cp_width_50">
-                    <select class="form-control" name="end_minute" id="end_minute" onchange="updateMyProject.checkDateIsFilled(this, {{ $project->id }})">
-                        <option value='' disabled selected style='display:none;'>分</option>
-                        @for ($i = 0; $i <= 60; $i++)
-                        <option value="{{ sprintf('%02d', $i) }}"
-                            {{ old('end_minute', $project->end_date->minute) == sprintf('%02d', $i) ? 'selected' : '' }}>
-                            {{ $i }}
-                        </option>
-                        @endfor
-                    </select>
-                </div>
-            </div>
-        </div>
+        <input type="text" id="end_date" name="end_date" class="p-postal-code def_input_100p"
+            value="{{ old('end_date', optional($project)->end_date) }}" placeholder="（例）100000" oninput="updateMyProject.textInput(this, {{ $project->id }})">
     </div>
-
     <x-common.save_back_button />
 </form>
