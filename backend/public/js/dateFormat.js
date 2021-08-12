21a4176eaa8f79aec83601a1584af3596b79e171
arakakitatsuki <20yuteo@gmail.com>
2021-08-12 16:00:39 +0900

    add date format js

---
+++ backend/public/js/dateFormat.js
@@ -0,0 +1,9 @@
+const config = {
+    dateFormat: "Y-m-d H:i",
+    enableTime: true
+};
+flatpickr("#start_date", config);
+
+flatpickr("#end_date", config);
+
+flatpickr(".delivery_date", { dateFormat: "Y-m-d" });
