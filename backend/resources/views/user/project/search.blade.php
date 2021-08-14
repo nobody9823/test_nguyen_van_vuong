0c0dc435a8a96a306899bda71b9751114cf45362
Yuzuki-9 <s1f102000624@iniad.org>
2021-08-14 15:21:50 +0900

    tweak
    CU-d34vx2

--- backend/resources/views/user/project/search.blade.php
+++ backend/resources/views/user/project/search.blade.php
@@ -158,40 +158,10 @@
 <script src="{{ asset('/js/clear-search-entry.js') }}"></script>
 
 <script>
-    const selectedRadioButtonHandler = () => {
-        let tagId = document.querySelector("input[name='tag_id']:checked")?.value;
-        let sortType = document.querySelector("input[name='sort_type']:checked")?.value;
-        window.location.replace(`?tag_id=${tagId}&sort_type=${sortType}`);
-    };
-    // プロジェクトのお気に入り登録・解除処理
-    $('.liked_project').on('click', function() {
-        var el = $(this);
-        var projectId = el.attr('id');
-
-        el.append('<meta name="csrf-token" content="{{ csrf_token() }}">');
-
-        $.ajax({
-            url: '/project/'+ projectId + '/liked',
-            type: 'POST',
-            data: {'project_id': projectId },
-            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-        })
-        .then((res) => {
-            console.log(res);
-            if(res == "登録"){
-                el.children('i').attr('class', 'fas fa-heart project-like-icon');
-            } else if(res == "削除"){
-                el.children('i').attr('class', 'far fa-heart project-like-icon');
-            } else if (res == "未ログイン") {
-                alert("ログインしてください");
-            } else {
-                alert("エラーが起こりました。");
-            }
-        })
-        .fail((error)=>{
-            console.log("エラーが起こりました。")
-        })
-    });
+    // csrfトークンを下記のproject-liked.jsファイルに送信
+    window.Laravel = {};
+    window.Laravel.csrfToken = @json( csrf_token() );
 </script>
+<script src="{{ asset('/js/project-liked.js') }}"></script>
 
 @endsection
