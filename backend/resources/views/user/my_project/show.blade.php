080134f3f748cacd89f92524ddb416444eb8247a
adgjmptwgw <adgjmptwgw@gmail.com>
2021-08-14 22:03:05 +0900

    Tweak

--- backend/resources/views/user/my_project/show.blade.php
+++ backend/resources/views/user/my_project/show.blade.php
@@ -148,18 +148,5 @@
                     </div>
             </div>
     </div>
-    <div class="pager E-font">
-        <ul class="pagination">
-            {{-- @if ($payments->previousPageUrl() !== null)
-                <li class="pager_pre"><a href="{{ $payments->previousPageUrl() }}"><span>«</span></a></li>
-            @endif
-            @foreach ($payments->links()->elements[0] as $key => $link)
-                <li><a href="{{ $link }}" class="{{ $payments->currentPage() == $key ? 'pager_active' : ''}}"><span>{{ $key }}</span></a></li>
-            @endforeach
-            @if ($payments->nextPageUrl() !== null)
-                <li class="pager_next"><a href="{{ $payments->nextPageUrl() }}"><span>»</span></a></li>
-            @endif --}}
-        </ul>
-    </div>
 </section>
 @endsection
