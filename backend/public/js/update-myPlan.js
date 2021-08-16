c1ae0ce58cd2f4390260df67142386148863532d
arakakitatsuki <20yuteo@gmail.com>
2021-08-16 21:58:32 +0900

    fix to display error message at update my plan js

--- backend/public/js/update-myPlan.js
+++ backend/public/js/update-myPlan.js
@@ -22,13 +22,19 @@ const updateMyPlan = (() => {
         }
     }
 
-    const displayError = message => {
-        document.getElementById('errors_delivery_date').innerHTML = message;
-        setTimeout(() => { disappearError(); }, 5000 );
+    const displayError = (data, message, planId) => {
+        const ul = document.createElement('ul');
+        message.forEach(e => {
+            var childElement = document.createElement('li');
+            childElement.innerHTML = e;
+            ul.appendChild(childElement);
+        });
+        document.getElementById('errors_return_' + (Object.keys(data)[0]) + (planId !== undefined ? '_' + planId : '')).appendChild(ul);
+        setTimeout(() => { disappearError(data, planId); }, 5000 );
     }
 
-    const disappearError = () => {
-        document.getElementById('errors_delivery_date').innerHTML = "";
+    const disappearError = (data, planId) => {
+        document.getElementById('errors_return_' + (Object.keys(data)[0] + (planId !== undefined ? '_' + planId : ''))).innerHTML = "";
     }
 
     const displayIcon = (data, planId) => {
@@ -81,18 +87,17 @@ const updateMyPlan = (() => {
 
         axios.post(`/my_project/project/${projectId}/updatePlan/${planId === undefined ? document.getElementById('plan_id').value : planId}`, data).then(res => {
             spinner.style.display = 'none';
-            displayIcon(data, planId);
-
             if(res.data.result === true){
                 if (data instanceof FormData){
                     previewUploadedImage(data, planId);
                 }
+                displayIcon(data, planId);
             } else if (res.data.message !== undefined){
-                displayError(res.data.message.delivery_date);
+                console.log(data);
+                displayError(data, res.data.message[Object.keys(data)[0]], planId);
             }
         }).catch(res => {
             spinner.style.display = 'none';
-            displayIcon(data, planId);
             console.log(res);
         });
     }
