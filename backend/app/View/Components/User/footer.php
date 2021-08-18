81033eda335c708636d2b7c9145d3ee1a991d2e9
Yuzuki-9 <s1f102000624@iniad.org>
2021-08-18 18:09:29 +0900

    Component footer to pass tags to be used in the footer
    CU-d35kg5

---
+++ backend/app/View/Components/User/footer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components\User;
+
+use Illuminate\View\Component;
+use App\Models\Tag;
+
+class footer extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function tags()
+    {
+        return Tag::all();
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.user.footer');
+    }
+}
