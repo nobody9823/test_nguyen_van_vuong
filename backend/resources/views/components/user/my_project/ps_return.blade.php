b5f519a0ee60fae9b13edacf4d47d6359aaba8d3
ShinnosukeTakagi <s.takagi5664@gmail.com>
2021-08-20 10:04:29 +0900

    Improvement successful saved message at ps_return.blade
    feature/CU-e00zrr

--- backend/resources/views/components/user/my_project/ps_return.blade.php
+++ backend/resources/views/components/user/my_project/ps_return.blade.php
@@ -31,21 +31,21 @@
     <div class="ps_plan_form_wrapper">
         <div class="ps_plan_form_item">
             <div class="form_item_tit">支援総額順リターン内容<span class="hissu_txt">必須</span><span class="disclaimer">※300文字以内で入力してください</span></div>
+            <textarea name="reward_by_total_amount" class="def_textarea tiny_editor" rows="6">{{ old('reward_by_total_amount', optional($project)->reward_by_total_amount) }}</textarea>
             <div class="spinner-wrapper">
                 <div class="spinner" id="spinner_reward_by_total_amount"></div>
-                <i class="fa fa-check-circle green" aria-hidden="true" style="display: none;" id="saved_reward_by_total_amount"></i>
+                <p class="saved_icon" aria-hidden="true" style="display: none;" id="saved_reward_by_total_amount">保存しました</p>
                 <span id="errors_reward_by_total_amount" style="color: red;"></span>
             </div>
-            <textarea name="reward_by_total_amount" class="def_textarea tiny_editor" rows="6">{{ old('reward_by_total_amount', optional($project)->reward_by_total_amount) }}</textarea>
         </div>
         <div class="ps_plan_form_item">
             <div class="form_item_tit">支援件数順リターン内容<span class="hissu_txt">必須</span><span class="disclaimer">※300文字以内で入力してください</span></div>
+            <textarea name="reward_by_total_quantity" class="def_textarea tiny_editor" rows="6">{{ old('reward_by_total_quantity', optional($project)->reward_by_total_quantity) }}</textarea>
             <div class="spinner-wrapper">
                 <div class="spinner" id="spinner_reward_by_total_quantity"></div>
-                <i class="fa fa-check-circle green" aria-hidden="true" style="display: none;" id="saved_reward_by_total_quantity"></i>
+                <p class="saved_icon" aria-hidden="true" style="display: none;" id="saved_reward_by_total_quantity">保存しました</p>
                 <span id="errors_reward_by_total_quantity" style="color: red;"></span>
             </div>
-            <textarea name="reward_by_total_quantity" class="def_textarea tiny_editor" rows="6">{{ old('reward_by_total_quantity', optional($project)->reward_by_total_quantity) }}</textarea>
         </div>
     </div>
 
