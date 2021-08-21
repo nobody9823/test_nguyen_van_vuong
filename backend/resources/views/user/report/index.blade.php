0692e233322d16b7930979486ca143fbbfb41aff
adgjmptwgw <adgjmptwgw@gmail.com>
2021-08-21 19:44:04 +0900

    Fixed the mobile screen style of the activity report list screen

--- backend/resources/views/user/report/index.blade.php
+++ backend/resources/views/user/report/index.blade.php
@@ -10,13 +10,13 @@
         <div class="sub_tit_L">活動報告一覧</div>
     </div>
 
-    <div class="prof_page_base inner_item">
+    <div class="prof_page_base inner_item reports">
         <div class="prof_page_L">
             <x-user.mypage-navigation-bar/>
         </div>
 
         <div class="prof_page_R">
-            <table class="report_table">
+            <table>
                 <tr>
                     <th>作成日</th>
                     <!-- TODO:こちらは将来必要になった際に追加する -->
@@ -29,24 +29,25 @@
                     <tr class="prof_edit_row">
                         <td>
                             <div>{{ $report->created_at->format('Y年m月d日') }}<br>
-                            <span>{{ $report->created_at->format('H:m') }}</span></div>  
+                            <span>{{ $report->created_at->format('H:m') }}</span></div>
                         </td>
                         <td>
                           {{ $report->title }}
                         </td>
-                        <td class="report_icons">
-                            <!-- <i class="fas fa-ellipsis-h fa-lg"></i>
-                            <i class="far fa-trash-alt"></i> -->
-                            <i class="far fa-edit fa-lg"></i>
+                        <td>
+                            <div>
+                                <p class="mobile_display">{{ $report->created_at->format('Y年m月d日 H:m') }}</p>
+                                <i class="far fa-edit fa-lg"></i>
+                            </div>
                         </td>
                     </tr>
                     @endforeach
                 @endif
             </table>
             
-            <div class="def_btn" style="margin: 100px 170px;">
+            <div class="def_btn">
               <button type="submit" class="disable-btn">
-                <p style="font-size: 1.8rem;font-weight: bold;color: #fff;">投稿する</p>
+                <p style="font-size: 1.8rem;font-weight: bold;color: #fff;">新規投稿を作成</p>
               </button>
             </div>
 
@@ -59,33 +60,61 @@
 @endsection
 
 <style>
-  .report_table tr:first-child {
+  /* テーブル */
+  .reports tr:first-child {
     width: 100%;
     display: flex;
     background: #F7FDFF;
     color: #00AEBD;
   }
 
-  .report_table tr:first-child th {
+  .reports tr:first-child th {
     margin: 20px;
   }
 
-  .report_table th:nth-child(2) {
+  .reports th:nth-child(2) {
     width: 535px;
   }
 
-  .prof_edit_row td:nth-child(2) {
+  .reports .prof_edit_row td:nth-child(2) {
     width: 400px;
   }
-  
-  .prof_edit_row td {
-    height: 100px;
-    display: flex;
-    align-items: center;
+
+  /* アイコン */
+  .reports .mobile_display { display: none; }
+
+  /* 新規投稿ボタン */
+  .reports .def_btn { margin: 70px 170px 100px; }
+
+  /* スマホサイズ */
+  @media (max-width: 767px) {
+    /* テーブルの各スタイル */
+    .reports tr:first-child { width: calc(100% - 20%); display: none;)}
+    .reports .prof_edit_row td { height: 60px; }
+    .reports .prof_edit_row td:first-child { display: none; }
+    .reports .prof_edit_row td:nth-child(2) { width:100%; margin-top: 25px;}
+    .reports .prof_edit_row td:nth-child(3) div { color: #00AEBD; font-size: 85%; }
+    /* アイコン */
+    .reports .fa-edit {
+    position: relative;
+    bottom: 20px;
+    left: 305px;
+    }
+    .reports .mobile_display { display: block; }
+    /* 新規投稿ボタン */
+    .reports .def_btn { margin: 50px 0px 100px; }
   }
 
-  .report_icons .fa-edit, .prof_edit_row td:nth-child(2) {
-    position: relative;
-    right: 25px;
+  /* PCサイズ */
+  @media (min-width: 767px) {
+    .reports .prof_edit_row td {
+      height: 100px;
+      display: flex;
+      align-items: center;
+    }
+    .reports .fa-edit, .reports .prof_edit_row td:nth-child(2) {
+      position: relative;
+      right: 25px;
+    }
   }
 </style>
