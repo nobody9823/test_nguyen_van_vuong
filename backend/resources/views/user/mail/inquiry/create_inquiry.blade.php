c3e49f16c4be444ee00d7ffd92c140d745dc75af
kosei-program-dev <kosei.program.dev@gmail.com>
2021-08-21 10:00:17 +0900

    implement for sp
    #CU-dzxy6n

--- backend/resources/views/user/mail/inquiry/create_inquiry.blade.php
+++ backend/resources/views/user/mail/inquiry/create_inquiry.blade.php
@@ -11,13 +11,16 @@
     <section id="" class="inquiry_attention_section">
         <div class="inquiry_section_inner inner_item">
             <div class="tit_L_01 E-font"><h2>CONTACT</h2><div class="sub_tit_L">お問い合わせ</div></div>
-            <p class="inquiry_attention">お問い合わせの前に必ずご確認ください</p>
-            <p class="inquiry_text">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-            <div class="inquiry_border"></div>
-            <p class="inquiry_attention">ご不明な点がありましたら、ヘルプページをご覧ください</p>
-            <p class="inquiry_anchor_wrapper"><a class='inquiry_anchor' href="">サポーター向けよくある質問</a></p>
-            <p class="inquiry_anchor_wrapper"><a class='inquiry_anchor' href="">プロジェクト実行者向けよくある質問</a></p>
-            <p class="inquiry_anchor_wrapper"><a class='inquiry_anchor' href="">ヘルプページ</a></p>
+            <div class="inquiry_attention_box">
+                <p class="inquiry_attention">お問い合わせの前に必ずご確認ください</p>
+                <p class="inquiry_text">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+            </div>
+            <div class="inquiry_transition_box">
+                <p class="inquiry_attention inquiry_attention_2">ご不明な点がありましたら、ヘルプページをご覧ください</p>
+                <p class="inquiry_anchor_wrapper"><a class='inquiry_anchor' href="">サポーター向けよくある質問</a></p>
+                <p class="inquiry_anchor_wrapper"><a class='inquiry_anchor' href="">プロジェクト実行者向けよくある質問</a></p>
+                <p class="inquiry_anchor_wrapper"><a class='inquiry_anchor' href="">ヘルプページ</a></p>
+            </div>
         </div>
     </section>
 
@@ -92,15 +95,15 @@
                         <textarea name="content" class="def_textarea" id="content" required>{{ old('content') }}</textarea>
                     </div>
                 </div>
-                <div class="inquiry_attention_border"></div>
-                <div>
+                {{-- <div class="inquiry_attention_border"></div> --}}
+                <div class="inquiry_notice_box">
                     <p>・内容により回答をおまたせする場合や、回答しかねる場合がございます。</p>
                     <p>・弊社からの回答は、弊社の許可なく、記事の全文または一部の無断転載および再配布はご遠慮いただいております。</p>
                 </div>
 
 
 
-                <div class="inquiry_attention_border"></div>
+                {{-- <div class="inquiry_attention_border"></div> --}}
     
                 {{-- <div class="form">
                     <label for="image-upload" class="control-label">添付ファイル</label>
@@ -118,7 +121,7 @@
                 {{-- <button type="submit" id="send" class="my-page_btn">問い合わせる</button> --}}
                 <div style='text-align:center'>
                     <input required type="checkbox" id="confirm" class="ac_list_checks" value="confirm">
-                    <label for="confirm" class="checkbox-fan">上記の前提事項、及び<a href="#">利用規約</a>の個人情報保護方針を確認しました</label>
+                    <label for="confirm" class="inquiry_confirm_text checkbox-fan">上記の前提事項、及び<a href="#">利用規約</a>の個人情報保護方針を確認しました</label>
         
                     <button type="submit" id="send" class="inquiry_button">問い合わせる</button>
                 </div>
