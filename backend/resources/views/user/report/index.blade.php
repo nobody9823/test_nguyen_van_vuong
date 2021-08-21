216d2fe93987f0879a2c010cb9aaaac2afe887ba
adgjmptwgw <adgjmptwgw@gmail.com>
2021-08-21 16:07:56 +0900

    Implemented the design of the activity report list page

--- backend/resources/views/user/report/index.blade.php
+++ backend/resources/views/user/report/index.blade.php
@@ -16,12 +16,61 @@
         </div><!--/prof_page_L-->
 
         <div class="prof_page_R">
-          @if($reports)
-            @foreach($reports as $key => $report)
-
-            @endforeach
-          @endif
+            <table class="report_table">
+                <tr>
+                    <th>作成日</th>
+                    <!-- こちらは将来必要になった際に追加する -->
+                    <!-- <th>公開設定</th> -->
+                    <th>タイトル</th>
+                    <th>操作</th>
+                </tr>
+                <tr class="prof_edit_row">
+                    <td>
+                        <div>2021年10月10日<br><span>10:10</span></div>  
+                    </td>
+                    <td>
+                      １２３４５６７８９０１２３４５６７８９０１２３４５１２３４５６７８９０１２３４５６７８９０１２３４５
+                    </td>
+                    <td>
+                        <i class="fas fa-ellipsis-h fa-lg"></i>
+                        <!-- <i class="far fa-edit"></i>
+                        <i class="far fa-trash-alt"></i> -->
+                    </td>
+                </tr>
+            </table>
+            
+            <div class="def_btn">
+              <button type="submit" class="disable-btn">
+                <p style="font-size: 1.8rem;font-weight: bold;color: #fff;">投稿する</p>
+              </button>
+            </div>
         </div>
     </div>
 </section>
 @endsection
+
+<style>
+  .report_table tr:first-child {
+    width: 100%;
+    display: flex;
+    background: #F7FDFF;
+    color: #00AEBD;
+  }
+
+  .report_table tr:first-child th {
+    margin: 20px;
+  }
+
+  .report_table th:nth-child(2) {
+    width: 535px;
+  }
+
+  .prof_edit_row td:nth-child(2) {
+    width: 400px;
+  }
+
+  .fa-ellipsis-h {
+    position: relative;
+    right: 25px;
+  }
+</style>
