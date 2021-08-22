13eaaa7d5def30e37eba07f10be755d983e4425a
adgjmptwgw <adgjmptwgw@gmail.com>
2021-08-22 15:06:37 +0900

    Implemented activity report update process

--- backend/app/Http/Controllers/User/ReportController.php
+++ backend/app/Http/Controllers/User/ReportController.php
@@ -46,4 +46,18 @@ class ReportController extends Controller
     {
         return view('user.report.edit', ['project' => $project, 'report' => $report]);
     }
+
+    public function update(ReportRequest $request, Project $project, Report $report)
+    {
+        DB::beginTransaction();
+        try {
+            $report->fill($request->fillWithProjectId($project->id))->save();
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            return redirect()->back()->withErrors('活動報告の作成に失敗しました。管理会社に連絡をお願いします。');
+        }
+
+        return redirect()->action([ReportController::class, 'index'], ['project' => $project])->with('flash_message', '更新が完了しました。');
+    }
 }
