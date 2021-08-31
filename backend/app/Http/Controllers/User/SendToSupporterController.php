4e7978527fa058b2e6078aec6feb8184ff272854
arakakitatsuki <20yuteo@gmail.com>
2021-08-31 11:45:34 +0900

    add send to supporter controller

---
+++ backend/app/Http/Controllers/User/SendToSupporterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SendToSupporterRequest;
+use App\Models\Payment;
+use App\Models\Project;
+use Illuminate\Support\Facades\DB;
+
+class SendToSupporterController extends Controller
+{
+    public function __invoke(Project $project, SendToSupporterRequest $request)
+    {
+        DB::beginTransaction();
+        try {
+            Payment::whereIn('id', $request->payment_ids)->update(['is_sent' => true]);
+            DB::commit();
+            return redirect()->action([SupporterController::class, 'index'], ['project' => $project])->with(['flash_message' => '発送済みに変更しました。']);
+        } catch (\Exception $e) {
+            DB::rollback();
+            throw $e;
+            return redirect()->action([SupporterController::class, 'index'], ['project' => $project])->withErrors(['エラーが発生しました。管理会社にご連絡をお願いします。']);
+        }
+
+    }
+}
