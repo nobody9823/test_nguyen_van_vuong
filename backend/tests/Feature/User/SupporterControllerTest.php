ad36b26756b9714b12ccf8049b19bef4ef9b66ab
arakakitatsuki <20yuteo@gmail.com>
2021-09-01 12:51:03 +0900

    add supporter controller test

---
+++ backend/tests/Feature/User/SupporterControllerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use App\Models\Payment;
+use App\Models\PaymentToken;
+use App\Models\Project;
+use App\Models\Plan;
+use App\Models\MessageContent;
+use App\Models\ProjectFile;
+use App\Models\User;
+use App\Models\Identification;
+use App\Models\Address;
+use App\Models\Profile;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SupporterControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->users = User::factory()
+            ->has(Identification::factory())
+            ->has(Address::factory())
+            ->has(Profile::factory())
+            ->has(
+                Project::factory()->released()
+                    ->has(
+                        ProjectFile::factory()->state([
+                            'file_url' => 'public/sampleImage/now_printing.png',
+                            'file_content_type' => 'image_url',
+                        ])
+                    )
+                    ->has(
+                        Plan::factory()->state([
+                            'price' => 1000
+                        ])
+                    )
+            )->count(10)->create();
+
+        $this->users->each(function (User $user){
+            $user->payments()->saveMany(
+                Payment::factory()->count(10)
+                    ->has(PaymentToken::factory())
+                    ->has(MessageContent::factory()->count(20))
+                    ->create()
+            );
+        });
+
+        Payment::all()->each(function (Payment $payment){
+            $payment->includedPlans()->attach(
+                [Plan::whereIn('project_id', Project::where('id', $payment->project_id)->select('id'))->inRandomOrder()->first()->id => ['quantity' => random_int(1, 3)]]
+            );
+        });
+
+        $this->user = $this->users->first();
+
+        $this->project = $this->user->projects()->first();
+    }
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testUserSupporterIndexPage()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->actingAs($this->user)->get(route('user.supporter.index', ['project' => $this->project]));
+
+        $response->assertStatus(200);
+    }
+}
