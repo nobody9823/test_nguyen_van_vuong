05df1fd3be7f0f4f8d01e1f39bf66722f9d567cf
adgjmptwgw <adgjmptwgw@gmail.com>
2021-09-02 13:22:52 +0900

    Implemented a test of PaymentHistory method

---
+++ backend/tests/Feature/User/MypageControllerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Project;
+use App\Models\Payment;
+use App\Models\PaymentToken;
+use App\Models\Plan;
+
+class MypageControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    Public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+
+        $this->project = Project::factory()->state([
+            'user_id' => $this->user->id,
+        ])->create();
+
+        $this->payment = Payment::factory()->state([
+            'user_id' => $this->user->id,
+            'project_id' => $this->project->id
+        ])->create();
+
+        $this->plan = Plan::factory()->state([
+            'project_id' => $this->project->id,
+        ])->create();
+
+        $this->payment_token = PaymentToken::factory()->state([
+            'payment_id' => $this->payment->id,            
+        ])->create();
+
+        $this->payment->includedPlans()->attach($this->plan->id ,['quantity' => 1]);
+    }
+
+    public function testPaymentHistory()
+    {
+        $this->withoutExceptionHandling();
+        
+        $response = $this->actingAs($this->user)
+                         ->from(route('user.profile'))
+                         ->get(route('user.payment_history'));
+        $response->assertOk()
+                 ->assertViewIs('user.mypage.payment')
+                 ->assertViewHas(['payments' ,'project']);
+    }
+}
