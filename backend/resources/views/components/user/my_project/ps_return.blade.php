542cac8a330518741f9a3ef445d221ca277e6e50
ShinnosukeTakagi <s.takagi5664@gmail.com>
2021-09-10 18:27:25 +0900

    Commented out reward_by_total_amount input textarea
    feature/CU-j3f9p7

--- backend/resources/views/components/user/my_project/ps_return.blade.php
+++ backend/resources/views/components/user/my_project/ps_return.blade.php
@@ -5,14 +5,15 @@
         <h4 style="color: #e65d65">以下の注意事項をご確認ください。</h4>
         <ul>
             <li>
-                PSリターン獲得の条件は、支援者がプロジェクトサポーター(PS)となってプロジェクトページへのリンクをSNSで紹介し、別のユーザーがそのリンクによって集まった支援の総額が「支援総額」購入されたリターンの合計個数が「支援件数」として、それぞれの順位によって獲得ができます。
+                {{-- PSリターン獲得の条件は、支援者がプロジェクトサポーター(PS)となってプロジェクトページへのリンクをSNSで紹介し、別のユーザーがそのリンクによって集まった支援の総額が「支援総額」購入されたリターンの合計個数が「支援件数」として、それぞれの順位によって獲得ができます。 --}}
+                PSリターン獲得の条件は、支援者がプロジェクトサポーター(PS)となってプロジェクトページへのリンクをSNSで紹介し、別のユーザーがそのリンクによって集まった支援の合計人数が「支援人数」として、その順位によって獲得ができます。
             </li>
             <li>
                 支援者の方により多くのSNSでの紹介を行っていただくために、PSリターンには通常のリターンとは違った特別なリターンを設定してください。
             <br/>
                 また、リターン内容はなるべく支援者にコストのかからないイベント/体験型のリターンを設定してください。
             <br/>
-                例) 紹介支援総額上位３名で、お礼イベント開催。
+                例) 紹介支援人数上位３名で、お礼イベント開催。
             </li>
             <li>
                 <a href="{{ route('user.my_project.reward_sample') }}" target="_blank">こちらからサンプルを確認できます。</a>
@@ -29,11 +30,11 @@
         </ul> --}}
     </div>
     <div class="ps_plan_form_wrapper">
-        <div class="ps_plan_form_item">
+        {{-- <div class="ps_plan_form_item">
             <div class="form_item_tit">支援総額順リターン内容<span class="hissu_txt">必須</span><span class="disclaimer">※300文字以内で入力してください</span></div>
             <textarea name="reward_by_total_amount" class="def_textarea tiny_editor" rows="6">{{ old('reward_by_total_amount', optional($project)->reward_by_total_amount) }}</textarea>
             <x-common.async-submit-message propName="reward_by_total_amount" />
-        </div>
+        </div> --}}
         <div class="ps_plan_form_item">
             <div class="form_item_tit">支援件数順リターン内容<span class="hissu_txt">必須</span><span class="disclaimer">※300文字以内で入力してください</span></div>
             <textarea name="reward_by_total_quantity" class="def_textarea tiny_editor" rows="6">{{ old('reward_by_total_quantity', optional($project)->reward_by_total_quantity) }}</textarea>
