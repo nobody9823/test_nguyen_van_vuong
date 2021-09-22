d7edf1892a9717111ccfd9dad6beaea705d7fbc2
adgjmptwgw <adgjmptwgw@gmail.com>
2021-09-22 14:17:24 +0900

    Tweak

--- backend/app/Http/Requests/ProjectRequest.php
+++ backend/app/Http/Requests/ProjectRequest.php
@@ -40,7 +40,7 @@ class ProjectRequest extends FormRequest
             'user_id' => ['required', 'integer'],
             'title' => ['required', 'string', 'max:255'],
             'content' => ['required', 'string', 'max:100000'], // 最大16,777,215文字（約16Mバイト）
-            // WARNING:支援総額順のPSリターンを使用する際はprepareForValidationのoffsetUnsetの処理を削除して下さい。
+            // WARNING:支援総額順のPSリターンを使用する際はprepareForValidationの空文字登録処理を削除して下さい。
             // 'reward_by_total_amount' => ['required', 'string', 'max:100000'], // 最大16,777,215文字（約16Mバイト）
             'reward_by_total_quantity' => ['required', 'string', 'max:100000'], // 最大16,777,215文字（約16Mバイト）
             // 'target_amount' => ['required', 'integer', 'min:10000', 'max:99999999'],
