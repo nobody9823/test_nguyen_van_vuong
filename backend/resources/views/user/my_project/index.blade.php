f45c1b2be94c125b0207de4eaabf4b91f1a66209
adgjmptwgw <adgjmptwgw@gmail.com>
2021-10-23 18:35:11 +0900

    Changed design project delete button

--- backend/resources/views/user/my_project/index.blade.php
+++ backend/resources/views/user/my_project/index.blade.php
@@ -17,6 +17,15 @@
                 <div class="my_project_container">
                     @foreach($projects as $project)
                     <div class="my_project_img_box_wrapper">
+                        @if ($project->release_status === ProjectReleaseStatus::getValue('Default') || $project->release_status === ProjectReleaseStatus::getValue('SendBack'))
+                        <div>
+                            <form action="{{ route('user.my_project.project.destroy', ['project' => $project]) }}" method="POST" onsubmit="return confirm('本当に削除しますか？')">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="fas fa-times-circle fa-2x project_delete_btn"></button>
+                            </form>
+                        </div>
+                        @endif
                         <div class="ib02_01 E-font my_project_img_wrapper">
                             <a href="{{ route('user.my_project.project.show', ['project' => $project]) }}">
                                 @if ($project->projectFiles()->where('file_content_type', 'image_url')->count() > 0)
@@ -28,11 +37,26 @@
                             {{-- NOTICE: MyProjectController, show action --}}
                         </div>
 
+                        <div class="ib02_03">
+                            <a href="{{ route('user.my_project.project.show', ['project' => $project]) }}">
+                                <h3>{{ Str::limit($project->title, 40) }}</h3>
+                                {{-- NOTICE: MyProjectController, show action--}}
+                            </a>
+                        </div>
+
+                        <div class="def_btn edit_btn">
+                            @if($project->release_status === '---' || $project->release_status === '差し戻し' || $project->release_status === '掲載停止中')
+                            編集
+                            {{-- NOTICE: MyProjectController, edit action --}}
+                            <a class="cover_link" href="{{ route('user.my_project.project.edit', ['project' => $project]) }}"></a>
+                            @elseif($project->release_status === '承認待ち' || $project->release_status === '掲載中')
+                            {{ $project->release_status }}
+                            <a class="cover_link"></a>
+                            @endif
+                        </div>
+
                         <div class="my_project_img_content_wrapper">
                             <div class="ib02_02 my_project_release_status">
-                                <div>
-                                ({{ $project->release_status === ProjectReleaseStatus::getValue('Default') ? '申請前' : $project->release_status }})
-                                </div>
                                 <div class="my_project_apply_wrapper">
                                     @if($project->release_status === ProjectReleaseStatus::getValue('Default') || $project->release_status === ProjectReleaseStatus::getValue('SendBack') || $project->release_status === ProjectReleaseStatus::getValue('UnderSuspension'))
                                     <div class="def_btn my_project_apply">
@@ -44,38 +68,8 @@
                                         </form>
                                     </div>
                                     @endif
-
-                                    @if ($project->release_status === ProjectReleaseStatus::getValue('Default') || $project->release_status === ProjectReleaseStatus::getValue('SendBack'))
-                                    <div class="def_btn my_project_apply">
-                                        <form action="{{ route('user.my_project.project.destroy', ['project' => $project]) }}" method="POST" onsubmit="return confirm('削除しますか？')">
-                                            @csrf
-                                            @method('DELETE')
-                                            削除する
-                                            <button type="submit" class="cover_link disable-btn">
-                                            </button>
-                                        </form>
-                                    </div>
-                                    @endif
                                 </div>
                             </div>
-
-                            <div class="ib02_03">
-                                <a href="{{ route('user.my_project.project.show', ['project' => $project]) }}">
-                                    <h3>{{ Str::limit($project->title, 46) }}</h3>
-                                    {{-- NOTICE: MyProjectController, show action--}}
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="def_btn">
-                            @if($project->release_status === '---' || $project->release_status === '差し戻し' || $project->release_status === '掲載停止中')
-                            編集
-                            {{-- NOTICE: MyProjectController, edit action --}}
-                            <a class="cover_link" href="{{ route('user.my_project.project.edit', ['project' => $project]) }}"></a>
-                            @elseif($project->release_status === '承認待ち' || $project->release_status === '掲載中')
-                            {{ $project->release_status }}
-                            <a class="cover_link"></a>
-                            @endif
                         </div>
                     </div><!--/.img_box_01_L_item-->
                     @endforeach
@@ -95,3 +89,27 @@
     </div>
 </section>
 @endsection
+
+<style>
+    #supported-projects .edit_btn{
+        width: 80%;
+    }
+
+    .project_delete_btn{
+        position: relative;
+        bottom: 15px;        
+        z-index: 1;
+        color: red;
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+    }
+
+    @media (max-width: 767px) {
+        .project_delete_btn { right: 25px; }
+    }
+    
+    @media (min-width: 1000px) {
+        .project_delete_btn { left: 275px; }
+    }
+</style>
