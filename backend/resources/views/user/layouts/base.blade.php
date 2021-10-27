a267360378f4175a7c7aa312a2a3a2cdfa526790
adgjmptwgw <adgjmptwgw@gmail.com>
2021-10-27 11:53:42 +0900

    Tweak

--- backend/resources/views/user/layouts/base.blade.php
+++ backend/resources/views/user/layouts/base.blade.php
@@ -205,30 +205,6 @@ if(isiOS) {
 
 			<!--▼ ★★★ログイン時-->
             @auth('web')
-			<!-- <li class="menuset_05 login_after_user_btn">
-				<div id="user_btn">
-					<div class="user_btn_01">
-                        <a href="{{ route('user.profile') }}">
-                            @if(isset(Auth::user()->profile))
-                            <div class="user_btn_01_01" style="background-image: url({{ Storage::url(Auth::user()->profile->image_url) }})">
-                            @else
-                            <div class="user_btn_01_01" style="background-image: url('image/my-page.svg')">
-                            @endif
-                            </div>
-                        </a>
-                        <div class="user_btn_01_02">{{ Auth::user()->name }}さん</div>
-                        <div class="user_btn_01_03">
-                            <a href="{{ route('user.profile') }}"><i class="fas fa-edit"></i></a>
-                        </div>
-                        <div class="user_btn_01_04">
-                            <form action="{{ route('logout') }}" method="POST">
-                                @csrf
-                                <button class="disable-btn" type="submit"><i class="fas fa-sign-out-alt"></i></button>
-                            </form>
-                        </div>
-					</div>
-				</div>
-			</li> -->
             <li class="menu-item nav_btn taso_li menuset_03 login_btn">
                 <a href="{{ route('user.profile') }}" class="top_menu-1 nav_btn_link">
                     <p class="nav_btn_tit_L">プロフィール</p>
