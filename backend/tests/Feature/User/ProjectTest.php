8eca62737bcab14fba2366bb24e16e3b2ad63a65
高木 晋之輔 <63362495+Shinnosuke-Takagi@users.noreply.github.com>
2021-11-11 18:18:31 +0900

    Revert "deploy to staging"

--- backend/tests/Feature/User/ProjectTest.php
+++ backend/tests/Feature/User/ProjectTest.php
@@ -151,7 +151,8 @@ class ProjectTest extends TestCase
         $response = $this->actingAs($this->user)
             ->get(route('user.plan.prepare_for_payment', array_merge(['project' => $this->project], $this->data_for_credit)));
 
-        $response->assertRedirect(route('user.plan.payment_for_credit', ['project' => $this->project, 'payment_without_globalscope' => $this->user->payments()->withoutGlobalScopes()->first()]));
+        $this->payment = $this->user->payments()->first();
+        $response->assertRedirect(route('user.plan.payment_for_credit', ['project' => $this->project, 'payment' => $this->payment]));
     }
 
     public function testPrepareForPaymentForPayPay()
@@ -169,7 +170,7 @@ class ProjectTest extends TestCase
             ->get(route('user.plan.prepare_for_payment', array_merge(['project' => $this->project], $this->data_for_paypay)));
 
         $response->assertRedirect('https://qr-stg.sandbox.paypay.ne.jp/28180104c6BFhmBN9MGwhLaz');
-        $payments = $this->user->payments()->withoutGlobalScopes()->get();
+        $payments = $this->user->payments()->get();
         $this->assertCount(1, $payments);
     }
 }
