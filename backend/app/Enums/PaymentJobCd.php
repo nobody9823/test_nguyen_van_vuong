57f0c4c9a48a0a40be8d7cd24235c34793e9dec8
高木 晋之輔 <63362495+Shinnosuke-Takagi@users.noreply.github.com>
2021-11-12 16:27:42 +0900

    Revert "Revert "Revert "Revert "deploy to staging""""

---
+++ backend/app/Enums/PaymentJobCd.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static AUTH()
+ * @method static SALES()
+ * @method static CANCEL()
+ */
+final class PaymentJobCd extends Enum
+{
+    const DEFAULT = '---';
+    const AUTH = '仮売上';
+    const SALES = '実売上';
+    const VOID = 'キャンセル';
+}
