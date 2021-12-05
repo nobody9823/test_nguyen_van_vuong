e1fc09d1804af543ded04bba3124fa32366dbf00
ShinnosukeTakagi <s.takagi5664@gmail.com>
2021-12-05 15:12:54 +0900

    Tiny fix

--- backend/resources/views/user/mail/template/payment_finished.blade.php
+++ backend/resources/views/user/mail/template/payment_finished.blade.php
@@ -5,6 +5,7 @@
         <div>Fan Returnです。</div>
         <div>ご支援ありがとうございます。</div>
         <div>あなたは、{{ $billing_users_count }}人目の支援者です。</div>
+        <div>現在の支援総額は¥ {{ $payments_sum_price }}です。</div>
         <div>プロジェクト名 : {{ $project_title }}</div>
         <div>オーダーID : {{ $payment_id }}</div>
     </p>
