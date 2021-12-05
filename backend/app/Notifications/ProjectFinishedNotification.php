46a3ed2b3df7f501e0b2535b597a33c20f10b414
ShinnosukeTakagi <s.takagi5664@gmail.com>
2021-12-05 14:58:58 +0900

    Implemented schedule event to send notification to admin, project author and supporters when project end_date is past
    feature/CU-1wfkagh

---
+++ backend/app/Notifications/ProjectFinishedNotification.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\AnonymousNotifiable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ProjectFinishedNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($project)
+    {
+        $this->project = $project->getLoadIncludedPaymentsCountAndSumPrice();
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $address = $notifiable instanceof AnonymousNotifiable
+            ? $notifiable->routeNotificationFor('mail')
+            : $notifiable->email;
+
+        $url = $address === $this->project->user->email
+            ? route('user.my_project.project.show', ['project' => $this->project])
+            : route('user.project.show', ['project' => $this->project]);
+
+        return (new MailMessage)
+            ->subject('【FanReturn】プロジェクトの掲載が終了しました。')
+            ->view(
+                'user.mail.template.project_is_finished',
+                ['project' => $this->project, 'url' => $url]
+            );
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
