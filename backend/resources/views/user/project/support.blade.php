7a08cfdd302d516d954d4db3d9d1dfa820890263
adgjmptwgw <adgjmptwgw@gmail.com>
2021-12-08 13:28:30 +0900

    Corrected due to change in remaining time calculation logic

--- backend/resources/views/user/project/support.blade.php
+++ backend/resources/views/user/project/support.blade.php
@@ -32,18 +32,9 @@
                     <div>現在の支援金額：{{ number_format($project->payments_sum_price) }}円</div>
                     <div>現在の支援者数：{{ $project->payments_count }}人</div>
                     @if (DateFormat::checkDateIsFuture($project->start_date))
-                        {{-- NOTICE: 追加開発が決まったらコメントアウトを外してください --}}
-                        {{-- @if (DateFormat::checkDateIsWithInADay($project->start_date))
-                            <div style="color: #e65d65;">募集開始まで残り：{{ DateFormat::getDiffCompareWithToday($project->start_date) }}時間</div>
-                        @else --}}
-                            <div>募集開始まで残り：{{ DateFormat::getDiffCompareWithToday($project->start_date) }}日</div>
-                        {{-- @endif --}}
+                        <div>募集開始まで残り：{{ DateFormat::getDiffCompareWithToday($project->start_date) }}</div>
                     @elseif (DateFormat::checkDateIsPast($project->start_date) && DateFormat::checkDateIsFuture($project->end_date))
-                        {{-- @if (DateFormat::checkDateIsWithInADay($project->end_date))
-                            <div style="color: #e65d65;">募集終了まで残り：{{ DateFormat::getDiffCompareWithToday($project->end_date) }}時間</div>
-                        @else --}}
-                            <div>募集終了まで残り：{{ DateFormat::getDiffCompareWithToday($project->end_date) }}日</div>
-                        {{-- @endif --}}
+                        <div>募集終了まで残り：{{ DateFormat::getDiffCompareWithToday($project->end_date) }}</div>
                     @elseif (DateFormat::checkDateIsPast($project->end_date))
                         <div>募集終了</div>
                     @endif
