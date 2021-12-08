a3bdfcee191e011a1c87d104bc00512a0d177ac8
adgjmptwgw <adgjmptwgw@gmail.com>
2021-12-08 15:43:56 +0900

    Button spacing
    feature/CU-1wuxteb[completed]

--- backend/resources/views/components/common/navigating_page_buttons.blade.php
+++ backend/resources/views/components/common/navigating_page_buttons.blade.php
@@ -1,29 +1,29 @@
 <!-- マイプロジェクトの「保存する」「プロジェクト一覧へ戻る」ボタン -->
 
 @if(isset($nextPageButtonForReturn))
-<div class="def_btn">
+<div class="def_btn btn_margin_bottom">
     <a style="font-size: 1.8rem;font-weight: bold;color: #fff; display: block" href="{{ route('user.my_project.project.edit', ['project' => $project, 'next_tab' => 'ps_return']) }}">
         次へ進む
     </a>
 </div>
 @elseif (!isset($nextPageButton))
-<div class="def_btn">
+<div class="def_btn btn_margin_bottom">
     <button type="submit" class="disable-btn">
         <p style="font-size: 1.8rem;font-weight: bold;color: #fff;">次へ進む</p>
     </button>
 </div>
 @endif
 
-<div class="def_btn">
+<div class="def_btn btn_margin_bottom">
     <a style="font-size: 1.8rem;font-weight: bold;color: #fff; display: block" href="{{ route('user.my_project.project.index') }}">
         プロジェクト一覧へ戻る
     </a>
 </div>
 
 <!-- @if (isset($previewForPsReturn))
-<div class="def_btn">
-    <a 
-        style="font-size: 1.8rem;font-weight: bold;color: #fff; display: block" 
+<div class="def_btn btn_margin_bottom">
+    <a
+        style="font-size: 1.8rem;font-weight: bold;color: #fff; display: block"
         href="{{ route('user.my_project.reward_preview', ['project' => $project])}}"
         target="_blank"
     >
@@ -31,9 +31,9 @@
     </a>
 </div>
 @else
-<div class="def_btn">
-    <a 
-        style="font-size: 1.8rem;font-weight: bold;color: #fff; display: block" 
+<div class="def_btn btn_margin_bottom">
+    <a
+        style="font-size: 1.8rem;font-weight: bold;color: #fff; display: block"
         href="{{ route('user.project_preview', ['project' => $project] )}}"
         target="_blank"
     >
