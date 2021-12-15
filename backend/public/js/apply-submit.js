2cbdeb38679b93f8ed0e1fb9efce184644f53291
adgjmptwgw <adgjmptwgw@gmail.com>
2021-12-15 13:05:54 +0900

    test2

--- backend/public/js/apply-submit.js
+++ backend/public/js/apply-submit.js
@@ -101,8 +101,8 @@ const applySubmit = (
     let joinedRequiredFields = requiredFields.join('');
 
     if (requiredFields.length === 0) {
-        // confirm('申請してもよろしいですか？') &&
-            document.getElementById('apply_form').submit();
+        alert('申請してもよろしいですか？');
+        document.getElementById('apply_form').submit();
     } else {
         let validationMessage = '下記の項目を正しく入力してから申請してください。\n' + joinedRequiredFields;
         alert(validationMessage);
