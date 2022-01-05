3a4faaf49456cd18ee881cce895eba66bce34117
ShinnosukeTakagi <s.takagi5664@gmail.com>
2022-01-06 00:38:35 +0900

    Fixed image url to include asset function

--- backend/resources/views/components/user/supporter-description.blade.php
+++ backend/resources/views/components/user/supporter-description.blade.php
@@ -9,18 +9,18 @@
             <div class="ps_desc_row">
                 <div class="ps_desc_L_tit">プロジェクトサポーターの方はこちら<div>プロジェクトを支援したい人</div>
                 </div>
-                <div class="ps_desc_img"><img src="/image/fan_psd_L_01.svg"></div>
-                <div class="ps_desc_img ps_desc_img_a"><img src="/image/fan_psd_arrow.svg"></div>
+                <div class="ps_desc_img"><img src="{{asset('image/fan_psd_L_01.svg')}}"></div>
+                <div class="ps_desc_img ps_desc_img_a"><img src="{{asset('image/fan_psd_arrow.svg')}}"></div>
                 <div class="ps_desc_txt">下記にある招待リンクをコピーし、SNSなどに貼り付け、<br>一緒に支援していただける方を募集
                     <br><span>※下記にある招待リンクから支援された場合にのみカウントされます</span>
                 </div>
-                <div class="ps_desc_img"><img src="/image/fan_psd_L_02.svg"></div>
-                <div class="ps_desc_img ps_desc_img_a"><img src="/image/fan_psd_arrow.svg"></div>
+                <div class="ps_desc_img"><img src="{{asset('image/fan_psd_L_02.svg')}}"></div>
+                <div class="ps_desc_img ps_desc_img_a"><img src="{{ asset('image/fan_psd_arrow.svg') }}"></div>
                 <div class="ps_desc_txt">各プロジェクトサポーターが発行した招待リンクによる支援総額と<br>件数によってランキングが表示される</div>
-                <div class="ps_desc_img"><img src="/image/fan_psd_L_03.svg"></div>
-                <div class="ps_desc_img ps_desc_img_a"><img src="/image/fan_psd_arrow.svg"></div>
+                <div class="ps_desc_img"><img src="{{asset('image/fan_psd_L_03.svg')}}"></div>
+                <div class="ps_desc_img ps_desc_img_a"><img src="{{ asset('image/fan_psd_arrow.svg') }}"></div>
                 <div class="ps_desc_txt">プロジェクトサポーターとして<br>特別リターンをGET</div>
-                <div class="ps_desc_img"><img src="/image/fan_psd_L_04.svg"></div>
+                <div class="ps_desc_img"><img src="{{asset('image/fan_psd_L_04.svg')}}"></div>
             </div>
             <div class="ps_desc_arrow"></div>
         </div>
@@ -30,16 +30,16 @@
             <div class="ps_desc_row">
                 <div class="ps_desc_R_tit">紹介された方はこちら<div>プロジェクトサポーターに紹介された人</div>
                 </div>
-                <div class="ps_desc_img"><img src="/image/fan_psd_R_01.svg"></div>
-                <div class="ps_desc_img ps_desc_img_a"><img src="/image/fan_psd_arrow.svg"></div>
+                <div class="ps_desc_img"><img src="{{asset('image/fan_psd_R_01.svg')}}"></div>
+                <div class="ps_desc_img ps_desc_img_a"><img src="{{ asset('image/fan_psd_arrow.svg') }}"></div>
                 <div class="ps_desc_txt">プロジェクトサポーターに紹介されて<br>プロジェクトを一緒に支援する</div>
-                <div class="ps_desc_img"><img src="/image/fan_psd_R_02.svg"></div>
-                <div class="ps_desc_img ps_desc_img_a"><img src="/image/fan_psd_arrow.svg"></div>
+                <div class="ps_desc_img"><img src="{{asset('image/fan_psd_R_02.svg')}}"></div>
+                <div class="ps_desc_img ps_desc_img_a"><img src="{{ asset('image/fan_psd_arrow.svg') }}"></div>
                 <div class="ps_desc_txt">プロジェクト実行者に支援する</div>
-                <div class="ps_desc_img"><img src="/image/fan_psd_R_03.svg"></div>
-                <div class="ps_desc_img ps_desc_img_a"><img src="/image/fan_psd_arrow.svg"></div>
+                <div class="ps_desc_img"><img src="{{asset('image/fan_psd_R_03.svg')}}"></div>
+                <div class="ps_desc_img ps_desc_img_a"><img src="{{ asset('image/fan_psd_arrow.svg') }}"></div>
                 <div class="ps_desc_txt">支援することで<br>リターンGET</div>
-                <div class="ps_desc_img"><img src="/image/fan_psd_R_04.svg"></div>
+                <div class="ps_desc_img"><img src="{{asset('image/fan_psd_R_04.svg')}}"></div>
             </div>
             <div class="ps_desc_arrow"></div>
         </div>
