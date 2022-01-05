5eb53d9a39efd3fe91d4b3be027e095f75ddc607
ShinnosukeTakagi <s.takagi5664@gmail.com>
2022-01-06 00:07:56 +0900

    Fixed storage link base url

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -38,7 +38,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('STORAGE_URL') . '/storage',
             'visibility' => 'public',
         ],
 
