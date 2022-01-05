4fea23d317322ab8c67662e23dab624d49d80a75
ShinnosukeTakagi <s.takagi5664@gmail.com>
2022-01-05 16:36:13 +0900

    Fixed failed test

--- backend/tests/Unit/Test.php
+++ backend/tests/Unit/Test.php
@@ -13,7 +13,7 @@ class Test extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->get('/fanding');
         $response->assertStatus(200);
     }
 }
