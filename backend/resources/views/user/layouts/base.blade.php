f05a63cb7a147897278253d763d52b6b17f149d7
ShinnosukeTakagi <s.takagi5664@gmail.com>
2022-01-19 21:52:47 +0900

    Tiny fix

--- backend/resources/views/user/layouts/base.blade.php
+++ backend/resources/views/user/layouts/base.blade.php
@@ -1,6 +1,24 @@
 <!doctype html>
 <html>
 <head>
+@production
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-217549034-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-217549034-1');
+</script>
+{{-- Microsoft Clarity --}}
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "a90vi2gpyj");
+</script>
+@endproduction
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <script>
