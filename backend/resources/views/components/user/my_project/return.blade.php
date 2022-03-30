d3868b7fd43aa91998bca5a88fdaa9f75a86441d
ShinnosukeTakagi <s.takagi5664@gmail.com>
2022-03-30 13:20:40 +0900

    Tiny fixed

--- backend/resources/views/components/user/my_project/return.blade.php
+++ backend/resources/views/components/user/my_project/return.blade.php
@@ -1,5 +1,6 @@
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
 <script>
 $(function() {
 	// リターンの並び替え
