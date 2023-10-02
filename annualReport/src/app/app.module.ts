ca93993b49945aef5671de84164e7812fd73f4be
vinh.nguyentuan <tuanvinhtl@gmail.com>
2023-10-02 22:51:56 +0700

    initial commit

--- annualReport/src/app/app.module.ts
+++ annualReport/src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { ButtonNavComponent } from "./components/button-nav.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, ButtonNavComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ annualReport/src/app/components/button-nav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-btn-nav",
+  templateUrl: "./button-nav.component.html",
+  styleUrls: ["./button-nav.component.css"],
+})
+export class ButtonNavComponent {}
